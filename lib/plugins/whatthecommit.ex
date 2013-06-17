a29ba73a1ce4d9ee3e739238d9af6a41e13d0de4
mrshankly <mrshankly@riseup.net>
2013-06-17 12:11:00 +0100

    Change wtc to a private function.

--- lib/plugins/whatthecommit.ex
+++ lib/plugins/whatthecommit.ex
@@ -1,7 +1,7 @@
 defmodule Whatthecommit do
   use GenEvent.Behaviour
 
-  def wtc(callback) do
+  defp wtc(callback) do
     url = 'http://whatthecommit.com/index.txt'
     case :httpc.request(:get, {url, []}, [], body_format: :binary) do
       {:ok, {{_, 200, _}, _, body}} ->
@@ -19,11 +19,11 @@ defmodule Whatthecommit do
     case msg do
       ["!wtc"] ->
         callback = fn(x) ->
-                     :gen_server.cast(gen_server, {:send_text, x})
+                       :gen_server.cast(gen_server, {:send_txt, x})
                    end
         spawn(fn() -> wtc(callback) end)
         {:ok, state}
-      _->
+      _ ->
         {:ok, state}
     end
   end
