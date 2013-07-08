d0d2f1307faaaa6049678d6a545af50b923b926f
mrshankly <mrshankly@riseup.net>
2013-07-08 08:40:35 +0100

    Fixed a crash when a message with an invalid atom was sent.

--- lib/plugins/cannedreplies.ex
+++ lib/plugins/cannedreplies.ex
@@ -1,31 +1,25 @@
 defmodule Cannedreplies do
   use GenEvent.Behaviour
 
-  @responses [gface: "( ≖‿≖)",
-              cool: "COOL LIKE SNOWMAN ☃",
-              chownface: "( ´· ‿ ·`)",
-              goface: "ʕ ◔ϖ◔ʔ",
-              edgyface: "(ケ≖‿≖)ケ",
-              dface: "ಠ_ಠ"]
-
-  @id elem(Tsmambo.Lib.consult("settings.cfg"), 1)[:bot_id]
+  @responses [{"gface", "( ≖‿≖)"},
+              {"cool", "COOL LIKE SNOWMAN ☃"},
+              {"chownface", "( ´· ‿ ·`)"},
+              {"goface", "ʕ ◔ϖ◔ʔ"},
+              {"edgyface", "(ケ≖‿≖)ケ"},
+              {"dface", "ಠ_ಠ"}]
 
   def init(_args) do
     {:ok, []}
   end
 
-  def handle_event({_, _, _, @id}, state) do
-    {:ok, state}
-  end
-
   def handle_event({msg, _user, _userid}, state) do
     case msg do
-      [s] ->
-        key = binary_to_atom(s)
-        if Dict.has_key?(@responses, key) do
-          :gen_server.cast(:mambo, {:send_txt, @responses[key]})
-          {:ok, state}
-        else
+      [key] ->
+        case ListDict.get(@responses, key) do
+          nil ->
+            {:ok, state}
+          reply ->
+            :gen_server.cast(:mambo, {:send_txt, reply})
             {:ok, state}
         end
       _ ->
