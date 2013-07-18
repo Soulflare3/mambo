d4065666bcbc8f70483e66d9e2789de7b78a1482
mrshankly <mrshankly@riseup.net>
2013-07-18 21:25:20 +0100

    Added fortune plugin.

---
+++ lib/plugins/fortune.ex
@@ -0,0 +1,28 @@
+defmodule Fortune do
+  use GenEvent.Behaviour
+
+  defp get_fortune(fortunes) do
+      i = :random.uniform(1615)
+      Enum.at fortunes, i
+  end
+
+  def init(_args) do
+    {:ok, ff} = File.read "fortunes"
+    ff = String.split ff, "\n"
+    {:ok, ff}
+  end
+
+  def handle_event({msg, _user, _userid}, fortunes) do
+    case msg do
+      ["!fortune"] ->
+        :gen_server.cast(:mambo, {:send_txt, get_fortune(fortunes)})
+        {:ok, fortunes}
+       _ ->
+        {:ok, fortunes}
+    end
+  end
+
+  def handle_event(_other, state) do
+    {:ok, state}
+  end
+end
